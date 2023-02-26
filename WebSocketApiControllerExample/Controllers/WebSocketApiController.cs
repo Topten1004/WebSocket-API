6482758fe922bbeb05e6b874ad02387756f5aa8a
TopTen1004 <97684719+TopTen1004@users.noreply.github.com>
2023-02-26 11:02:02 -0500

    initial version

---
+++ WebSocketApiControllerExample/Controllers/WebSocketApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketApiControllerExample.Controllers
+{
+    [Route("/api/ws")]
+    [ApiController]
+    public class WebSocketApiController : ControllerBase
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly IConnectionManager _connectionManager;
+
+        public WebSocketApiController(
+            IConnectionFactory connectionFactory, 
+            IConnectionManager connectionManager)
+        {
+            _connectionFactory = connectionFactory;
+            _connectionManager = connectionManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var context = ControllerContext.HttpContext;
+
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                Console.WriteLine($"Accepted connection '{context.Connection.Id}'");
+                var connection = _connectionFactory.CreateConnection(webSocket);
+                await _connectionManager.HandleConnection(connection);
+
+                return new EmptyResult();
+            }
+            else
+            {
+                return new StatusCodeResult((int) HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ WebSocketApiControllerExample/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebSocketApiControllerExample.Controllers;
+
+namespace WebSocketApiControllerExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionFactory = new ConnectionFactory();
+            var connectionManager = new ConnectionManager();
+            services.AddScoped(ctx => new WebSocketApiController(connectionFactory, connectionManager));
+            services.AddControllers().AddControllersAsServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseWebSockets(new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(30) });
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
